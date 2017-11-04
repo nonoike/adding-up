9bdf8c38b9a253c76202b831709a1935e74b6ec6
nonoike <ij4nu8d4fw@gmail.com>
2017-11-05 00:28:20 +0900

    データの選別処理実装

--- app.js
+++ app.js
@@ -9,8 +9,18 @@ const rl = readline.createInterface({ 'input': rs, 'output': {} });
 
 // lineイベントが発生したら処理をする
 rl.on('line', (lineString) => {
-    console.log(lineString);
-})
+    // 行をカンマで分割し、必要なカラムを保持する
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = columns[2];
+    const population = parseInt(columns[7]);
+    // 対象データを絞り込む
+    if (year === 2010 || year === 2015) {
+        console.log(year);
+        console.log(prefecture);
+        console.log(population);
+    }
+});
 
 // ストリームに情報を流す
 rl.resume();
