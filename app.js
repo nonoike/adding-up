89478c2723864fd6586a84a075e5468157b90f6f
nonoike <ij4nu8d4fw@gmail.com>
2017-11-05 00:22:18 +0900

    ファイルを１行ずつ読み込む

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+// モジュール呼び出し
+const fs = require('fs');
+const readline = require('readline');
+
+// ファイル読み込み
+const rs = fs.ReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ 'input': rs, 'output': {} });
+
+// lineイベントが発生したら処理をする
+rl.on('line', (lineString) => {
+    console.log(lineString);
+})
+
+// ストリームに情報を流す
+rl.resume();
